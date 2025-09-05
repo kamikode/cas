c53922fcff180b088ef58b599a2196b34b9df1f0
kamikode <oliver.unke@googlemail.com>
2025-09-05 23:18:37 +0200

    remove implicit cloning with + operator

--- src/ast.rs
+++ src/ast.rs
@@ -22,30 +22,6 @@ impl Add<Expression> for Expression {
     }
 }
 
-impl Add<&Expression> for Expression {
-    type Output = Expression;
-
-    fn add(self, other: &Expression) -> Expression {
-        Expression::Add(Box::new(self), Box::new(other.clone()))
-    }
-}
-
-impl Add<Expression> for &Expression {
-    type Output = Expression;
-
-    fn add(self, other: Expression) -> Expression {
-        Expression::Add(Box::new(self.clone()), Box::new(other))
-    }
-}
-
-impl Add<&Expression> for &Expression {
-    type Output = Expression;
-
-    fn add(self, other: &Expression) -> Expression {
-        Expression::Add(Box::new(self.clone()), Box::new(other.clone()))
-    }
-}
-
 impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -81,9 +57,6 @@ mod tests {
         let x = Expression::Symbol(Symbol::try_from("x".to_string()).unwrap());
         let y = Expression::Symbol(Symbol::try_from("y".to_string()).unwrap());
         let expected = Expression::Add(Box::new(x.clone()), Box::new(y.clone()));
-        assert_eq!(x.clone() + y.clone(), expected);
-        assert_eq!(x.clone() + &y, expected);
-        assert_eq!(&x + y.clone(), expected);
-        assert_eq!(&x + &y, expected);
+        assert_eq!(x + y, expected);
     }
 }
